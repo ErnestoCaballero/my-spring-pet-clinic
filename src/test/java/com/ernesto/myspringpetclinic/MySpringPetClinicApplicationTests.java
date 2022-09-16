c8fea74afe2413367b2dd36039d8c878e98e53fc
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-09-16 00:13:34 -0500

    start of project

---
+++ src/test/java/com/ernesto/myspringpetclinic/MySpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ernesto.myspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
