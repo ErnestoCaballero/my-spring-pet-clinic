a2dbc67d4c5b89dd8b9cb5910a05ee1bbc376241
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-09-23 00:57:38 -0500

    add base entity. closes #10

--- pet-clinic-data/src/main/java/com/ernesto/myspringpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/ernesto/myspringpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.ernesto.myspringpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/ernesto/myspringpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/ernesto/myspringpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.ernesto.myspringpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
