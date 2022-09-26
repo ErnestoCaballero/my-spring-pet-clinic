59f79654fb16e35aefed704c93bf96ea4c482bcf
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-09-26 17:10:53 -0500

    Implement Map Based Services. closes #6
    Create the map package with the abstract class AbstractMapService that will use a HashMap to implement the persistence of data
    sort of an in-memory DB with common CRUD operations.
    Afterwards, create the services for Owner, Pet and Vet extending from the abstract class
    but implementing the CrudService interface, which they match. Calling super for the impl.

---
+++ pet-clinic-data/src/main/java/com/ernesto/myspringpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package com.ernesto.myspringpetclinic.services.map;
+
+import com.ernesto.myspringpetclinic.model.Owner;
+import com.ernesto.myspringpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
