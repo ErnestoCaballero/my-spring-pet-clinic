3f324ed3321d2600d7cf2d63af3ba37451d3aa90
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-09-27 00:37:59 -0500

    add components to issue #13

--- pet-clinic-web/src/main/java/com/ernesto/myspringpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ernesto/myspringpetclinic/controllers/OwnerController.java
@@ -1,2 +1,14 @@
-package com.ernesto.myspringpetclinic.controllers;public class OwnerController {
+package com.ernesto.myspringpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners() {
+        return "owners/index";
+    }
 }
