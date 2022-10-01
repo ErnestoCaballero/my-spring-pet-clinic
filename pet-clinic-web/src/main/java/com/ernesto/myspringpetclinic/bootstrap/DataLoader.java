1e28afcad18f30f5328cffd60b16c6e5d63ffd9d
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-10-01 02:16:46 -0500

    refactor map impl to abstract id generation. close #23

--- pet-clinic-web/src/main/java/com/ernesto/myspringpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/ernesto/myspringpetclinic/bootstrap/DataLoader.java
@@ -21,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Agustin");
         owner1.setLastName("Laje");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Nicolas");
         owner2.setLastName("Marquez");
 
@@ -37,14 +35,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Pablo");
         vet1.setLastName("Munoz");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Ignacio");
         vet2.setLastName("Allende");
 
