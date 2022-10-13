fe2da0e3ac6ca3478b7c2687397c028a01cf7bac
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-10-13 00:58:30 -0500

    added pets and contact ingo to owners on startup. closes #33

--- pet-clinic-web/src/main/java/com/ernesto/myspringpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/ernesto/myspringpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.ernesto.myspringpetclinic.bootstrap;
 
 import com.ernesto.myspringpetclinic.model.Owner;
+import com.ernesto.myspringpetclinic.model.Pet;
 import com.ernesto.myspringpetclinic.model.PetType;
 import com.ernesto.myspringpetclinic.model.Vet;
 import com.ernesto.myspringpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.ernesto.myspringpetclinic.services.PetTypeService;
 import com.ernesto.myspringpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -33,16 +36,35 @@ public class DataLoader implements CommandLineRunner {
         dog.setName("Cat");
         PetType saveCatPetType = petTypeService.save(cat);
 
-
         Owner owner1 = new Owner();
         owner1.setFirstName("Agustin");
         owner1.setLastName("Laje");
+        owner1.setAddress("De la plata 123");
+        owner1.setCity("Cordoba");
+        owner1.setTelephone("1231231234");
+
+        Pet agustinPet = new Pet();
+        agustinPet.setPetType(saveDogPetType);
+        agustinPet.setOwner(owner1);
+        agustinPet.setBirthDate(LocalDate.now());
+        agustinPet.setName("Marx");
+        owner1.getPets().add(agustinPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Nicolas");
         owner2.setLastName("Marquez");
+        owner2.setAddress("De la plata 123");
+        owner2.setCity("Cordoba");
+        owner2.setTelephone("4324324321");
+
+        Pet nicolasCat = new Pet();
+        nicolasCat.setName("Guevara");
+        nicolasCat.setOwner(owner2);
+        nicolasCat.setBirthDate(LocalDate.now());
+        nicolasCat.setPetType(saveCatPetType);
+        owner2.getPets().add(nicolasCat);
 
         ownerService.save(owner2);
 
