e02970777225cc3228e8c9c0160cacd403df79af
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-10-14 00:42:55 -0500

    add specialties map impl. closes #31

---
+++ pet-clinic-data/src/main/java/com/ernesto/myspringpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.ernesto.myspringpetclinic.services;
+
+import com.ernesto.myspringpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
